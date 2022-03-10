4f49bf9619b5c5bfda65b4b02979563afadf870b
Luis Felipe <1luisfelipe2@gmail.com>
2022-03-10 19:37:47 -0400

    ok

--- src/components/SearchFunction/index.js
+++ src/components/SearchFunction/index.js
@@ -1,7 +1,8 @@
 export const arraySearch = (array, keyword) => {
+    
     const searchTerm = keyword.toLowerCase()
-    return array.filter(value => {
-        return value.name.toLowerCase().match(new RegExp(searchTerm, 'g')) ||
-        value.level.toLowerCase().match(new RegExp(searchTerm, 'g'))
+    return array.filter((value) => {
+        return value.name.toLowerCase().includes(searchTerm) ||
+        value.level.toLowerCase().includes(searchTerm) 
     })
 }
