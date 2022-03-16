d8666d2e689d3497b300649f479d0255ef5e2702
Luis Felipe <1luisfelipe2@gmail.com>
2022-03-16 10:58:24 -0400

    try n.600

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('my-app/build'));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 app.listen(process.env.PORT || 3000, (err) => {
