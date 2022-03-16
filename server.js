4e35b5f99f79bcd6b764a6336778ebd2cc5dac95
Luis Felipe <1luisfelipe2@gmail.com>
2022-03-16 13:55:28 -0400

    add process.env.NODE_ENV

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const path = require('path')
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('my-app/build'));
+if (process.env.NODE_ENV) {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.get("*", (req, res) => {
